dc6333317110b39c6150dd8429ed5d6c57bf5fad
Sergey Novikov <synopticum@gmail.com>
2021-03-02 17:43:43 +0300

    Extract Menu

---
+++ client/src/components/Header/UserMenu/Menu/index.tsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+import { color } from 'src/components/GlobalStyle/theme';
+
+export const Menu = styled.ul`
+  position: absolute;
+  right: 0;
+  top: 60px;
+  width: 150px;
+  padding: 15px;
+  border-radius: 5px;
+  background-color: ${color('white-1')};
+`;
+
+export const MenuItem = styled.li`
+  white-space: nowrap;
+  overflow-x: hidden;
+  text-overflow: ellipsis;
+  margin-bottom: 5px;
+
+  &:last-of-type {
+    margin-bottom: 0;
+  }
+`;
--- client/src/components/Header/UserMenu/index.tsx
+++ client/src/components/Header/UserMenu/index.tsx
@@ -1,11 +1,12 @@
 import { authStore, userStore } from 'src/stores';
-import React, { useRef } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { observer } from 'mobx-react-lite';
 import styled from 'styled-components';
 import { color } from 'src/components/GlobalStyle/theme';
 import Login from 'src/components/Header/UserMenu/Login';
 import state from './state';
 import { useClickOutside } from 'src/components/App/use-outside-click';
+import { Menu, MenuItem } from 'src/components/Header/UserMenu/Menu';
 
 const StyledUserMenu = styled.span`
   position: relative;
@@ -22,27 +23,6 @@ const Avatar = styled.img`
   border-radius: 5px;
 `;
 
-const Menu = styled.ul`
-  position: absolute;
-  right: 0;
-  top: 60px;
-  width: 150px;
-  padding: 15px;
-  border-radius: 5px;
-  background-color: ${color('white-1')};
-`;
-
-const Item = styled.li`
-  white-space: nowrap;
-  overflow-x: hidden;
-  text-overflow: ellipsis;
-  margin-bottom: 5px;
-
-  &:last-of-type {
-    margin-bottom: 0;
-  }
-`;
-
 const UserMenu: React.FC = observer(() => {
   const menuRef = useRef(null);
 
@@ -50,10 +30,10 @@ const UserMenu: React.FC = observer(() => {
     state.isOpen = false;
   });
 
-  const toggleMenu = (e: React.MouseEvent): void => {
+  const toggleMenu = useCallback((e: React.MouseEvent): void => {
     e.stopPropagation();
     state.isOpen = !state.isOpen;
-  };
+  }, []);
 
   const { isLogged } = authStore;
   if (!isLogged) return <Login />;
@@ -67,10 +47,10 @@ const UserMenu: React.FC = observer(() => {
 
       {state.isOpen && (
         <Menu ref={menuRef}>
-          <Item>Привет, {data.firstName}</Item>
-          <Item>
+          <MenuItem>Привет, {data.firstName}</MenuItem>
+          <MenuItem>
             <Link onClick={(): void => authStore.logout()}>Выйти</Link>
-          </Item>
+          </MenuItem>
         </Menu>
       )}
     </StyledUserMenu>
