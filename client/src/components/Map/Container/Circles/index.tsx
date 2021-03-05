0a82d98b89ab5b2b08235a8b02aba282cafbf2c2
Sergey Novikov <synopticum@gmail.com>
2021-03-05 13:58:53 +0300

    Don't fetch objects twice

--- client/src/components/Map/Container/Circles/index.tsx
+++ client/src/components/Map/Container/Circles/index.tsx
@@ -4,20 +4,8 @@ import { objectsStore, mapStore } from 'src/stores';
 import { observer } from 'mobx-react-lite';
 import { drawCircles } from 'src/components/Map/Container/Circles/draw-circles';
 
-const StyledCircles = styled.div`
-  position: fixed;
-  left: 0;
-  top: 0;
-`;
-
 export const Circles: React.FC = observer(() => {
   const { isFetching, isDataLoaded, error, data } = objectsStore.apiData;
-
-  useEffect(() => {
-    if (!isDataLoaded) {
-      objectsStore.fetchApiData();
-    }
-  }, []);
 
   useEffect(() => {
     if (mapStore.map && isDataLoaded) drawCircles(mapStore.map, data);
