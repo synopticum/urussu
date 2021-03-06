69e9b5ac07dfe4328df4eafa6243685d68ef90e9
Sergey Novikov <synopticum@gmail.com>
2021-03-06 12:32:39 +0300

    Handle circles click

--- client/src/components/Map/Container/Circles/draw-circles.ts
+++ client/src/components/Map/Container/Circles/draw-circles.ts
@@ -1,16 +1,21 @@
 import { circle, Map } from 'leaflet';
 import { ObjectMapped } from 'src/stores/MapStore/EntitiesStore/ObjectStore/map';
+import { mapStore } from 'src/stores';
 
 const removeCurrentCircles = (): void => {};
 
 const addCirclesToMap = (map: Map, circles: ObjectMapped[]): void => {
+  const handleClick = (id: string): void => mapStore.setEntity({ type: 'object', id });
+
   circles.forEach(item => {
     const { coordinates, radius } = item;
 
     circle(coordinates[0], parseInt(radius), {
       color: 'rgb(255, 198, 0)',
       weight: 2,
-    }).addTo(map);
+    })
+      .on('click', () => handleClick(item.id))
+      .addTo(map);
   });
 };
 
