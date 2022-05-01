3ea0572caa71d83ecad58484f637b4196efc1bb2
Sergey Novikov <synopticum@gmail.com>
2022-05-01 10:46:31 +0300

    Stop only specific containers

--- deploy/restart.sh
+++ deploy/restart.sh
@@ -1,5 +1,9 @@
 # Stop all running containers
-docker stop $(docker ps -a -q)
+docker stop $(docker ps -q --filter ancestor=urussu_nginx)
+docker stop $(docker ps -q --filter ancestor=urussu_api)
+docker stop $(docker ps -q --filter ancestor=urussu_ssr)
+docker stop $(docker ps -q --filter ancestor=urussu_client)
+docker stop $(docker ps -q --filter ancestor=mongo)
 
 # Pull container updates
 docker pull synopticum/urussu-client
--- deploy/stop.sh
+++ deploy/stop.sh
@@ -1,2 +1,7 @@
-docker stop $(docker ps -a -q)
+docker stop $(docker ps -q --filter ancestor=urussu_nginx)
+docker stop $(docker ps -q --filter ancestor=urussu_api)
+docker stop $(docker ps -q --filter ancestor=urussu_ssr)
+docker stop $(docker ps -q --filter ancestor=urussu_client)
+docker stop $(docker ps -q --filter ancestor=mongo)
+
 docker system prune -a -f
